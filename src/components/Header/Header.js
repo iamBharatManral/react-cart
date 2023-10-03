5596c1549c3d9c670df2cb2445205575d1edec80
iamBharatManral <justbharatmanral@gmail.com>
2023-10-03 07:52:36 +0530

    ui: add link to github repo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './Header.module.css'
 import cart from '../../assets/images/cart.svg'
+import github from '../../assets/images/github.svg'
 const Header = ({totalItems}) => {
     return (
         <header className={styles.header}>
             <h1>Cartducer</h1>
+            <a href={'https://github.com/iamBharatManral/react-cart'} target={'_blank'} rel="noreferrer"><img src={github} alt="github"/></a>
             <div className={styles.cart}>
                 <div>{totalItems}</div>
                 <img src={cart} alt="cart"/>
